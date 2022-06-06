719479d0328475b18860bfbfb7f5c22fb35756b8
edmgonsou <gonsou23@hotmail.com>
2022-06-06 15:59:49 -0300

    Inserindo Atividades da aula 3

---
+++ AULAS_ATIVIDADES/aula3/logica/src/repeticao/MediaAlunos.java
@@ -0,0 +1,30 @@
+package repeticao;
+
+import java.util.Scanner;
+
+public class MediaAlunos {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		double total = 0;
+		int contador = 0;
+		
+		while(contador <= 9) {
+			System.out.printf("\nDigite a %d nota: \n", contador+1);
+			double nota = sc.nextDouble();
+			total += nota;
+			contador++;
+		}
+		double media = total / contador;
+		
+		System.out.printf("\n\nTotal de todas as notas é %.1f", total);
+		System.out.printf("\nTotal de alunos é %d", contador);
+		System.out.printf("\nMédia da turma é %.1f", media);
+		
+		sc.close();
+
+	}
+
+}
